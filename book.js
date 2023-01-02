3dae5c6de4247a3a23122b14cd7417a9ef5f5329
CB <digplan@outlook.comm>
2023-01-02 17:26:49 -0600

    21

--- book.js
+++ book.js
@@ -379,3 +379,43 @@ function threeSum(nums) {
     }
     return result
 }
+
+/* 21 Edit distance
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+*/
+function minDistance(word1, word2) {
+    const m = word1.length
+    const n = word2.length
+
+    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0))
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0) {
+                dp[i][j] = j
+            } else if (j === 0) {
+                dp[i][j] = i
+            } else if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = 1 + Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
+            }
+        }
+    }
+    return dp[m][n]
+}
